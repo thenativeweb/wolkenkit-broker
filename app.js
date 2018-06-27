0936e8a11bf0d9b7c591132aa326bd91af5d73fd
Golo Roden <golo.roden@thenativeweb.io>
2018-06-27 10:21:46 +0200

    Add missing global try/catch.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 const path = require('path');
 
-const processEnv = require('processenv'),
+const flaschenpost = require('flaschenpost'),
+      processEnv = require('processenv'),
       tailwind = require('tailwind'),
       WolkenkitApplication = require('wolkenkit-application');
 
@@ -16,81 +17,87 @@ const eventSequencer = require('./eventSequencer'),
       modelStore = require('./modelStore'),
       performReplay = require('./appLogic/performReplay');
 
+const loggerSystem = flaschenpost.getLogger();
+
 (async () => {
-  const app = tailwind.createApp({
-    identityProvider: {
-      /* eslint-disable no-process-env */
-      name: process.env.IDENTITYPROVIDER_NAME,
-      certificate: path.join(process.env.IDENTITYPROVIDER_CERTIFICATE, 'certificate.pem')
-      /* eslint-enable no-process-env */
-    },
-    profiling: {
-      host: processEnv('PROFILING_HOST'),
-      port: processEnv('PROFILING_PORT')
-    }
-  });
+  try {
+    const app = tailwind.createApp({
+      identityProvider: {
+        /* eslint-disable no-process-env */
+        name: process.env.IDENTITYPROVIDER_NAME,
+        certificate: path.join(process.env.IDENTITYPROVIDER_CERTIFICATE, 'certificate.pem')
+        /* eslint-enable no-process-env */
+      },
+      profiling: {
+        host: processEnv('PROFILING_HOST'),
+        port: processEnv('PROFILING_PORT')
+      }
+    });
 
-  const applicationDirectory = path.join(app.dirname, 'app');
-  const application = new WolkenkitApplication(applicationDirectory);
+    const applicationDirectory = path.join(app.dirname, 'app');
+    const application = new WolkenkitApplication(applicationDirectory);
 
-  const logger = app.services.getLogger();
+    const logger = app.services.getLogger();
 
-  await modelStore.initialize({
-    application: app.env('APPLICATION'),
-    eventSequencer,
-    readModel: application.readModel,
-    stores: {
-      lists: new ListStore({ url: app.env('LISTSTORE_URL'), eventSequencer })
-    }
-  });
+    await modelStore.initialize({
+      application: app.env('APPLICATION'),
+      eventSequencer,
+      readModel: application.readModel,
+      stores: {
+        lists: new ListStore({ url: app.env('LISTSTORE_URL'), eventSequencer })
+      }
+    });
 
-  await eventStore.initialize({
-    url: app.env('EVENTSTORE_URL'),
-    namespace: `${app.env('APPLICATION')}domain`
-  });
+    await eventStore.initialize({
+      url: app.env('EVENTSTORE_URL'),
+      namespace: `${app.env('APPLICATION')}domain`
+    });
 
-  await app.commandbus.use(new app.wires.commandbus.amqp.Sender({
-    url: app.env('COMMANDBUS_URL'),
-    application: app.env('APPLICATION')
-  }));
+    await app.commandbus.use(new app.wires.commandbus.amqp.Sender({
+      url: app.env('COMMANDBUS_URL'),
+      application: app.env('APPLICATION')
+    }));
 
-  const eventHandlingStrategies = getEventHandlingStrategies({ app, eventStore, modelStore, readModel: application.readModel });
+    const eventHandlingStrategies = getEventHandlingStrategies({ app, eventStore, modelStore, readModel: application.readModel });
 
-  const fromPosition = eventSequencer.getLowestProcessedPosition() + 1,
-        toPosition = undefined;
+    const fromPosition = eventSequencer.getLowestProcessedPosition() + 1,
+          toPosition = undefined;
 
-  logger.info('Initially replaying events...', { fromPosition });
+    logger.info('Initially replaying events...', { fromPosition });
 
-  await performReplay({
-    eventStore,
-    fromPosition,
-    toPosition,
-    async handleReplayedDomainEvent (replayedDomainEvent) {
-      await eventHandlingStrategies.proceed(replayedDomainEvent, { type: 'proceed', forward: false });
-    }
-  });
+    await performReplay({
+      eventStore,
+      fromPosition,
+      toPosition,
+      async handleReplayedDomainEvent (replayedDomainEvent) {
+        await eventHandlingStrategies.proceed(replayedDomainEvent, { type: 'proceed', forward: false });
+      }
+    });
 
-  logger.info('Successfully replayed events.');
+    logger.info('Successfully replayed events.');
 
-  await app.eventbus.use(new app.wires.eventbus.amqp.Receiver({
-    url: app.env('EVENTBUS_URL'),
-    application: app.env('APPLICATION')
-  }));
+    await app.eventbus.use(new app.wires.eventbus.amqp.Receiver({
+      url: app.env('EVENTBUS_URL'),
+      application: app.env('APPLICATION')
+    }));
 
-  await app.api.use(new app.wires.api.http.Server({
-    keys: app.env('API_KEYS'),
-    clientRegistry: 'wolkenkit',
-    host: app.env('API_HOST'),
-    port: app.env('API_PORT'),
-    corsOrigin: getCorsOrigin(app.env('API_CORS_ORIGIN')),
-    writeModel: application.configuration.writeModel,
-    readModel: application.configuration.readModel
-  }));
+    await app.api.use(new app.wires.api.http.Server({
+      keys: app.env('API_KEYS'),
+      clientRegistry: 'wolkenkit',
+      host: app.env('API_HOST'),
+      port: app.env('API_PORT'),
+      corsOrigin: getCorsOrigin(app.env('API_CORS_ORIGIN')),
+      writeModel: application.configuration.writeModel,
+      readModel: application.configuration.readModel
+    }));
 
-  await app.status.use(new app.wires.status.http.Server({
-    port: app.env('STATUS_PORT'),
-    corsOrigin: app.env('STATUS_CORS_ORIGIN')
-  }));
+    await app.status.use(new app.wires.status.http.Server({
+      port: app.env('STATUS_PORT'),
+      corsOrigin: app.env('STATUS_CORS_ORIGIN')
+    }));
 
-  logic({ app, eventSequencer, eventStore, modelStore, readModel: application.readModel });
+    logic({ app, eventSequencer, eventStore, modelStore, readModel: application.readModel });
+  } catch (ex) {
+    loggerSystem.fatal(ex.message);
+  }
 })();
